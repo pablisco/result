519747f6324c41451e08fcc939e2b550b2c3aed3
Pablo Alonso <Pablo.Alonso@moonpig.com>
2017-05-03 01:11:55 +0100

    Added retrolambda

--- core/src/main/java/com/pablisco/result/Result.java
+++ core/src/main/java/com/pablisco/result/Result.java
@@ -2,10 +2,8 @@ package com.pablisco.result;
 
 public abstract class Result<S, F> {
 
-    private static final Function FORWARD_FUNCTION = new ForwardFunctions();
-
     public static <S, F> Result<S, F> success(S success) {
-        return new Success<S, F>(success);
+        return new Success<>(success);
     }
 
     public static <S, F> Result<S, F> failure(F failure) {
@@ -14,96 +12,53 @@ public abstract class Result<S, F> {
 
     public final boolean isSuccessful() {
         return fold(
-            Result.<S, Boolean>entity(true),
-            Result.<F, Boolean>entity(false)
+            o -> true,
+            o -> false
         );
     }
 
     public final S getSuccess() {
         return fold(
-            Result.<S>forward(),
-            Result.<F, S>unsupported("Can't get success from failure Result. Did you check isSuccessful()?")
+            t -> t,
+            unsupported("Can't get success from failure Result. Did you check isSuccessful()?")
         );
     }
 
     public final F getFailure() {
         return fold(
-            Result.<S, F>unsupported("Can't get failure from successful Result. Did you check isSuccessful()?"),
-            Result.<F>forward()
+            unsupported("Can't get failure from successful Result. Did you check isSuccessful()?"),
+            t -> t
         );
     }
 
     /**
      * Maps success to a new required success type, keeping the same failure type.
      */
-    public final <S2> Result<S2, F> map(final Function<S, S2> f) {
+    public final <S2> Result<S2, F> map(Function<S, S2> f) {
         return fold(
-            new Function<S, Result<S2, F>>() {
-                @Override
-                public Result<S2, F> apply(S s) {
-                    return success(f.apply(s));
-                }
-            },
-            new Function<F, Result<S2, F>>() {
-                @Override
-                public Result<S2, F> apply(F f) {
-                    return failure(f);
-                }
-            }
+            s -> success(f.apply(s)),
+            Result::failure
         );
     }
 
-    public final <S2> Result<S2, F> flatMap(final Function<S, Result<S2, F>> f) {
+    public final <S2> Result<S2, F> flatMap(Function<S, Result<S2, F>> onSuccess) {
         return fold(
-            new Function<S, Result<S2, F>>() {
-                @Override
-                public Result<S2, F> apply(S s) {
-                    return f.apply(s);
-                }
-            },
-            new Function<F, Result<S2, F>>() {
-                @Override
-                public Result<S2, F> apply(F f) {
-                    return failure(f);
-                }
-            }
+            onSuccess,
+            Result::failure
         );
     }
 
-    public final <S2, F2> Result<S2, F2> bimap(
-        final Function<S, Result<S2, F2>> onSuccess,
-        final Function<F, Result<S2, F2>> onFailure
-    ) {
+    public final <S2, F2> Result<S2, F2> bimap(Function<S, Result<S2, F2>> onSuccess, Function<F, Result<S2, F2>> onFailure) {
         return fold(
-            new Function<S, Result<S2, F2>>() {
-                @Override
-                public Result<S2, F2> apply(S s) {
-                    return onSuccess.apply(s);
-                }
-            },
-            new Function<F, Result<S2, F2>>() {
-                @Override
-                public Result<S2, F2> apply(F f) {
-                    return onFailure.apply(f);
-                }
-            }
+            onSuccess,
+            onFailure
         );
     }
 
     public final Result<F, S> swap() {
         return fold(
-            new Function<S, Result<F, S>>() {
-                @Override
-                public Result<F, S> apply(S s) {
-                    return failure(s);
-                }
-            },
-            new Function<F, Result<F, S>>() {
-                @Override
-                public Result<F, S> apply(F f) {
-                    return success(f);
-                }
-            }
+            Result::failure,
+            Result::success
         );
     }
 
@@ -140,61 +95,15 @@ public abstract class Result<S, F> {
     }
 
     /**
-     * Direct copy of the one use in Java 8
+     * Direct copy of the one use in Java 8 for Java 6 support
      */
+    @FunctionalInterface
     public interface Function<T, R> {
         R apply(T t);
     }
 
-    private static <T, R> Function<T, R> entity(R value) {
-        return new EntityFunction<T, R>(value);
-    }
-
     private static <T, R> Function<T, R> unsupported(String message) {
-        return new UnsupportedFunctions<T, R>(message);
-    }
-
-    @SuppressWarnings("unchecked")
-    private static <T> Function<T, T> forward() {
-        return (Function<T, T>) FORWARD_FUNCTION;
-    }
-
-    private static class EntityFunction<T, R> implements Function<T, R> {
-
-        private final R entity;
-
-        private EntityFunction(R entity) {
-            this.entity = entity;
-        }
-
-        @Override
-        public R apply(T o) {
-            return entity;
-        }
-    }
-
-    private static class UnsupportedFunctions<T, R> implements Function<T, R> {
-
-        private final String message;
-
-        private UnsupportedFunctions(String message) {
-            this.message = message;
-        }
-
-        @Override
-        public R apply(T t) {
-            throw new UnsupportedOperationException(message);
-        }
-
-    }
-
-    private static class ForwardFunctions<T> implements Function<T, T> {
-
-        @Override
-        public T apply(T t) {
-            return t;
-        }
-
+        return t -> { throw new UnsupportedOperationException(message);};
     }
 
 }
--- core/src/test/java/com/pablisco/result/ResultTest.java
+++ core/src/test/java/com/pablisco/result/ResultTest.java
@@ -10,7 +10,7 @@ public class ResultTest {
     private static final Character CHAR_VALUE = '9';
     private static final String SUCCESS_VALUE = "success";
     private static final String FAILURE_VALUE = "failure";
-    public static final int INTEGER_VALUE = 123;
+    private static final int INTEGER_VALUE = 123;
 
     @Test
     public void shouldBeSuccessful_withSuccess() throws Exception {
@@ -50,24 +50,13 @@ public class ResultTest {
         } catch (UnsupportedOperationException e) {
             assertThat(e).hasMessage("Can't get success from failure Result. Did you check isSuccessful()?");
         }
-
     }
 
     @Test
     public void shouldTransformSuccess() throws Exception {
         Result<Character, String> result = Result.<String, String>success(SUCCESS_VALUE)
-            .map(new Result.Function<String, Integer>() {
-                @Override
-                public Integer apply(String s) {
-                    return INTEGER_VALUE;
-                }
-            })
-            .map(new Result.Function<Integer, Character>() {
-                @Override
-                public Character apply(Integer character) {
-                    return CHAR_VALUE;
-                }
-            });
+            .map(s -> INTEGER_VALUE)
+            .map(character -> CHAR_VALUE);
 
         assertThat(result.isSuccessful()).isTrue();
         assertThat(result.getSuccess()).isEqualTo(CHAR_VALUE);
@@ -76,12 +65,7 @@ public class ResultTest {
     @Test
     public void shouldRemainFailure_whenMaping() throws Exception {
         Result<Character, String> result = Result.<String, String>failure(FAILURE_VALUE)
-            .map(new Result.Function<String, Character>() {
-                @Override
-                public Character apply(String s) {
-                    return CHAR_VALUE;
-                }
-            });
+            .map(s -> CHAR_VALUE);
 
         assertThat(result.isSuccessful()).isFalse();
         assertThat(result.getFailure()).isEqualTo(FAILURE_VALUE);
@@ -90,12 +74,7 @@ public class ResultTest {
     @Test
     public void shouldTransformSuccess_withFailure() throws Exception {
         Result<Character, String> failure = Result.<String, String>failure(FAILURE_VALUE)
-            .flatMap(new Result.Function<String, Result<Character, String>>() {
-                @Override
-                public Result<Character, String> apply(String s) {
-                    return Result.success(CHAR_VALUE);
-                }
-            });
+            .flatMap(s -> Result.success(CHAR_VALUE));
 
         assertThat(failure.isSuccessful()).isFalse();
         assertThat(failure.getFailure()).isEqualTo(FAILURE_VALUE);
@@ -104,12 +83,7 @@ public class ResultTest {
     @Test
     public void shouldNotTransformSuccess_intoSuccess() throws Exception {
         Result<String, String> result = Result.<String, String>success("first success")
-            .flatMap(new Result.Function<String, Result<String, String>>() {
-                @Override
-                public Result<String, String> apply(String s) {
-                    return Result.success(SUCCESS_VALUE);
-                }
-            });
+            .flatMap(s -> Result.success(SUCCESS_VALUE));
 
         assertThat(result.isSuccessful()).isTrue();
         assertThat(result.getSuccess()).isEqualTo(SUCCESS_VALUE);
@@ -119,18 +93,8 @@ public class ResultTest {
     public void shouldTransformToFailure_fromSuccess() throws Exception {
         Result<String, Character> result = Result.<String, String>success(SUCCESS_VALUE)
             .bimap(
-                new Result.Function<String, Result<String, Character>>() {
-                    @Override
-                    public Result<String, Character> apply(String s) {
-                        return Result.failure(CHAR_VALUE);
-                    }
-                },
-                new Result.Function<String, Result<String, Character>>() {
-                    @Override
-                    public Result<String, Character> apply(String s) {
-                        return Result.failure(Character.MIN_VALUE);
-                    }
-                }
+                s -> Result.failure(CHAR_VALUE),
+                f -> Result.failure(Character.MIN_VALUE)
             );
 
         assertThat(result.isSuccessful()).isFalse();
@@ -141,18 +105,8 @@ public class ResultTest {
     public void shouldTransformToSuccess_fromFailure() throws Exception {
         Result<String, Character> result = Result.<String, String>failure(FAILURE_VALUE)
             .bimap(
-                new Result.Function<String, Result<String, Character>>() {
-                    @Override
-                    public Result<String, Character> apply(String s) {
-                        return Result.success(SUCCESS_VALUE);
-                    }
-                },
-                new Result.Function<String, Result<String, Character>>() {
-                    @Override
-                    public Result<String, Character> apply(String character) {
-                        return Result.success(SUCCESS_VALUE);
-                    }
-                }
+                s -> Result.success(SUCCESS_VALUE),
+                f -> Result.success(SUCCESS_VALUE)
             );
 
         assertThat(result.isSuccessful()).isTrue();
